7f623f7302ff250af77d88969493604ffd3c10e2
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-07-26 15:31:37 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Simulate an API call that takes 3 seconds
     setTimeout(() => {
       if (Math.random() > 0.5) {
         resolve('Success!');
