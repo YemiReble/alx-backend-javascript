81267819c142d94f34dd889b01c835a968dea850
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-07-28 14:33:37 +0000

    Version Update

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let employeeNames = "";
+  for (const employee of reportWithIterator) {
+    employeeNames += employee + " | ";
+  }
+  return employeeNames.slice(0, -1);
+}
