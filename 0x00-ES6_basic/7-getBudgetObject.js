981e6fd76436898d2aa9062cbb6e3dde6e339498
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-07-28 10:31:25 +0000

    Get Function Object

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,3 @@
-export default function getBudgetObject(income, gdp, capital) {
-  return {'income': income, 'gdp': gdp, 'capita': capital}
-}
-
-//export default const getBudgetObject = (income, gdp, capita) => ({
-//	  income,
-//	  gdp,
-//	  capita,
-//});
+export default function getBudgetObject(income, gdp, capita) {
+  return {income: income, gdp: gdp, capita: capita}
+};
