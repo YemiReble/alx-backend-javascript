5edf039ac8f1f94438c8758a17194c6f9535d7ae
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-02 16:41:10 +0100

    Solution

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,7 @@
-// Define the weakMap constant
-const weakMap = new WeakMap();
-
-// Define the queryAPI function
-function queryAPI(endpoint) {
-  // Log the endpoint
-  console.log(`queryAPI: Calling endpoint ${endpoint}`);
-
-  // Track the number of queries for the endpoint in the weakMap
-  weakMap.set(endpoint, (previousCount) => previousCount + 1);
-
-  // Check if the number of queries is >= 5
-  if (weakMap.get(endpoint) >= 5) {
-    throw new Error(`Endpoint load is high: ${endpoint}`);
-  }
-
-  // Do the actual API call and log the response
-  fetch(`https://api.example.com/${endpoint}`)
-    .then((response) => response.json())
-    .then((data) => console.log(`queryAPI: API response for ${endpoint}:`, data));
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  let called = 0;
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  weakMap.set(endpoint, called + 1);
+  if (called + 1 >= 5) throw new Error('Endpoint load is high');
 }
