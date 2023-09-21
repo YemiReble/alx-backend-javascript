a2af2e3786a9dcd868f2eb1ce63a3107245f06bb
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-21 17:22:10 +0100

    Version Update

---
+++ 0x05-Node_JS_basic/Automation-Files/nodejs_installation.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+    echo
+    echo "${red}================================================================================${normal}"
+    echo "${red}================================================================================${normal}"
+    echo
+    echo -e "  ${bold}${yellow}${title}${normal}"
+    echo
+    echo -en "  ${text}"
+    echo
+    echo "${red}================================================================================${normal}"
+    echo "Success"
+
+# sudo apt-get remove nodejs
+# sudo apt-get update
+
+# This is to Install version 12.16. As at the time of this installation
+# Version 18.0.0 was the latest.
+# curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+# sudo bash nodesource_setup.sh
+
+# Install NodeJS
+# sudo apt install nodejs -y
+
+
+# To Install and Configure Coding Style for all JavaScript. See: https://eslint.org/
+# npm init @eslint/config
+
+# To install Jest for Testing in Development area: Learn More about Jest
+# npm i jest -D
